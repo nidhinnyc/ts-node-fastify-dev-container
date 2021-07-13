f32ede0ed53052c5ee5968c0e69848d0ae21b56e
Nidhin Surendran <nidhins@gmail.com>
2021-07-13 22:42:16 +0530

    linting fix

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,15 +1,15 @@
-import fastify from 'fastify'
+import fastify from 'fastify';
 
-const server = fastify()
+const server = fastify();
 
 server.get('/ping', async (request, reply) => {
-  return 'pong\n'
-})
+	return 'pong\n';
+});
 
 server.listen(8080, '0.0.0.0',(err, address) => {
-  if (err) {
-    console.error(err)
-    process.exit(1)
-  }
-  console.log(`Node Server listening at ${address}`)
-})
+	if (err) {
+		console.error(err);
+		process.exit(1);
+	}
+	console.log(`Node Server listening at ${address}`);
+});
